c59e7d15477198a8649c190a0230dffb4c5b8f3d
Matthew R. Wilson <mwilson@mattwilson.org>
2021-05-15 23:58:35 -0700

    greyscale PNG output

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 marsbrot: main.c
-	cc -o marsbrot main.c
+	cc -o marsbrot -I/opt/homebrew/include -L/opt/homebrew/lib -lpng main.c
--- main.c
+++ main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <png.h>
 
-#define W 80
-#define H 40
+#define W 1920
+#define H 1080
+
+void setRGB(png_byte *ptr, int val);
+int writeImage(char* filename, int width, int height, int *buffer, char* title);
+
+int maxIterations = 100;
 
 int main(int argc, char **argv)
 {
 	int w, h, y, x;
 	double minr, maxr, mini, maxi;
 	double rfactor, ifactor;
-	int maxIterations = 50;
 
 	w = W;
 	h = H;
-	int image[W][H];
+	int *image;
+
+	image = (int *) malloc(w*h*sizeof(int));
+	if (image == NULL) {
+		printf("Couldn't allocate image buffer.\n");
+		return 1;
+	}
 
 	double centerx = -0.5;
 	double centery = 0;
 
-	// At a zoom of 1, we want a horizontal span of 3.0.
+	// At a zoom of 1, we want a horizontal span of 5.0.
 	double zoom = 1.0;
-	double hspan = 3.0 / zoom;
+	double hspan = 4.5 / zoom;
 	double minx = centerx - (hspan / 2.0);
 	double maxx = centerx + (hspan / 2.0);
 
@@ -52,34 +64,123 @@ int main(int argc, char **argv)
 			double c_re = minr + x * rfactor;
 
 			double z_re = c_re, z_im = c_im; // Set z = c
-			int isInside = 1;
+			int isinside = 1;
+			int itercount;
 			for (int n = 0; n < maxIterations; n++)
 			{
 				if (z_re * z_re + z_im * z_im > 4)
 				{
-					isInside = 0;
+					itercount = n;
+					isinside = 0;
 					break;
 				}
 				double z_im2 = z_im * z_im;
 				z_im = 2 * z_re * z_im + c_im;
 				z_re = z_re * z_re - z_im2 + c_re;
 			}
-			image[x][y] = isInside;
+			if(isinside) {
+				image[y*w + x] = -1;
+			} else {
+				image[y*w + x] = itercount;
+			}
 		}
 	}
 
-	for (y = 0; y < h; y++)
-	{
-		for (x = 0; x < w; x++)
-		{
-			if (image[x][y])
-				printf("*");
-			else
-				printf(" ");
-		}
-		printf("\n");
-	}
+	writeImage("output.png", w, h, image, "Mandelbrot");
 
 	return 0;
 }
 
+/* PNG write code from:
+ * http://www.labbookpages.co.uk/software/imgProc/libPNG.html
+ */
+int writeImage(char* filename, int width, int height, int *buffer, char* title)
+{
+	int code = 0;
+	FILE *fp = NULL;
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
+	png_bytep row = NULL;
+	
+	// Open file for writing (binary mode)
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open file %s for writing\n", filename);
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Setup Exception handling
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		fprintf(stderr, "Error during png creation\n");
+		code = 1;
+		goto finalise;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	// Write header (8 bit colour depth)
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+			8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+			PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	// Set title
+	if (title != NULL) {
+		png_text title_text;
+		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+		title_text.key = "Title";
+		title_text.text = title;
+		png_set_text(png_ptr, info_ptr, &title_text, 1);
+	}
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Allocate memory for one row (3 bytes per pixel - RGB)
+	row = (png_bytep) malloc(3 * width * sizeof(png_byte));
+
+	// Write image data
+	int x, y;
+	for (y=0 ; y<height ; y++) {
+		for (x=0 ; x<width ; x++) {
+			setRGB(&(row[x*3]), buffer[y*width + x]);
+		}
+		png_write_row(png_ptr, row);
+	}
+
+	// End write
+	png_write_end(png_ptr, NULL);
+
+	finalise:
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+	if (row != NULL) free(row);
+
+	return code;
+}
+
+void setRGB(png_byte *ptr, int val)
+{
+	if (val == -1) {
+		ptr[0] = 0; ptr[1] = 0; ptr[2] = 0;
+	} else {
+		int ccc = (int)(255.0*((double)val/(double)maxIterations));
+		ptr[0] = ccc; ptr[1] = ccc; ptr[2] = ccc;
+	}
+}
